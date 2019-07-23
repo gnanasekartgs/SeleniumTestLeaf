80e692d9328d481f89d95c5be99b1514956d502d
gnanasekar <gnanasekar69@gmail.com>
2019-07-23 08:50:47 +0530

    refactor

--- Orbita/src/main/java/Demo/DragDrop.java
+++ Orbita/src/main/java/Demo/DragDrop.java
@@ -9,13 +9,30 @@ public class DragDrop {
 	public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 	ChromeDriver d = new ChromeDriver();
-	d.get("https://jqueryui.com/droppable/");
-	d.switchTo().frame(0);
+	//d.get("https://jqueryui.com/droppable/");
+	d.get("https://www.testandquiz.com/selenium/testing.html");
 	Actions builder = new Actions(d);
+	WebElement target = d.findElementById("dblClkBtn");
+	builder.doubleClick(target).perform();
+	d.switchTo().alert().accept();
 	
-	WebElement source = d.findElementById("draggable");
-	WebElement dest = d.findElementById("droppable");
-		builder.dragAndDrop(source, dest).perform();
+	d.findElementByXPath("//button[text()='Generate Alert Box']").click();
+	String text = d.switchTo().alert().getText();
+	System.out.println(text);
+	d.switchTo().alert().accept();
+	
+	
+	
+	
+	
+	
+	
+//	d.switchTo().frame(0);
+//	
+//	WebElement source = d.findElementById("draggable");
+//	WebElement dest = d.findElementById("droppable");
+//		builder.dragAndDrop(source, dest).perform();
+//		builder.dragAndDropBy(source, 10, 2).perform();
 	
 	}
 
--- Orbita/src/main/java/Sekar/Demo1.java
+++ Orbita/src/main/java/Sekar/Demo1.java
@@ -5,33 +5,41 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Demo1 {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		long startTime = System.currentTimeMillis();
+			
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver d = new ChromeDriver();
-		d.get("https://www.google.com/intl/en-GB/gmail/about/#");
+		d.get("https://mail.google.com");
 		d.manage().window().maximize();
-		d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		d.findElementByXPath("(//li[@class='h-c-header__nav-li g-mail-nav-links']//a)[2]").click();
 		
-//		WebDriverWait wait = new WebDriverWait(d, 10);
-//		WebElement until = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='email']")));
+		//d.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		
-	//	until.sendKeys("gnanasekar5500@gmail.com");
-		
-		
-		d.findElementByXPath("(//span[text()='to continue to Gmail']/following::input)[1]").sendKeys("gnanasekar5500@gmail.com");
-		
+		d.findElementByXPath("//input[@type='email']").sendKeys("gnanasekar5500@gmail.com");
 		d.findElementByXPath("//span[@class='RveJvd snByac']").click();
 		
-		d.findElementByXPath("//input[@type='password']").sendKeys("9994551183@Psk");
-		d.findElementByXPath("(//div[@class='ZFr60d CeoRYc'])[1]").click();
+		WebDriverWait wait = new WebDriverWait(d, 10);
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+		
+		d.findElementByXPath("//input[@type='password']").sendKeys("9790375500@Tgs");
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='ZFr60d CeoRYc'])[1]")));
+		d.findElementByXPath("//span[text()='Next']").click();
+		Thread.sleep(5000);
+		
+		Actions actions = new Actions(d);
+		WebElement elements = d.findElementByXPath("//a[text()='Inbox']");
+		actions.moveToElement(elements).perform();
+		String attribute = d.findElementByXPath("//a[text()='Inbox']").getText();
+		
+		//String attribute = d.findElementByXPath("//a[text()='Inbox']").getAttribute("title");
+		System.out.println(" "+attribute);
+		
+		
 		d.close();
 	}
 
